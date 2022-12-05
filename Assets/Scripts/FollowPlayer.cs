41f339d1205665bdf8ea108fe349bb0af167d8d3
Coralain10 <carolain.antoc@gmail.com>
2022-12-05 01:39:54 -0500

    FollowPlayer fixed

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player;
-    public int xMin;
-    public int xMax;
-
+    private GameObject player;
+    public Tilemap tileMap;
+    [SerializeField] float yOverflow;
+    short pixelSize = 24;
+    int xMin = 0;
+    int xMax = 0;
+    
     void Start()
     {
+        //tileMap = GetComponent<Tilemap>();
+        if (tileMap) {
+            xMax = tileMap.size.x - (Screen.width/pixelSize)/4;
+        }
         if (player == null) {
             player = GameObject.Find("Player");
         }
@@ -18,12 +26,12 @@ public class FollowPlayer : MonoBehaviour
     void LateUpdate()
     {
         if (player.transform.position.x < xMin) {
-            transform.position = new Vector3(xMin, player.transform.position.y, transform.position.z);
+            transform.position = new Vector3(xMin, player.transform.position.y + yOverflow, transform.position.z);
         }
         else if (player.transform.position.x > xMax) {
-            transform.position = new Vector3(xMax, player.transform.position.y, transform.position.z);
+            transform.position = new Vector3(xMax, player.transform.position.y + yOverflow, transform.position.z);
         } else {
-            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yOverflow, transform.position.z);
         }
     }
 }
